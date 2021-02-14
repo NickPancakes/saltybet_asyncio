753faad7d3930f79edc0433c27451c619146feae
Nick Pancakes <nick@pncks.com>
2021-02-14 14:02:43 -0800

    Export classes.

--- saltybet_asyncio/__init__.py
+++ saltybet_asyncio/__init__.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 __version__ = "0.1.0"
+
+from .client import SaltybetClient as SaltybetClient
+from .enums import (
+    BettingStatus as BettingStatus,
+    GameMode as GameMode,
+    BettingSide as BettingSide,
+    Tier as Tier,
+    Upgrade as Upgrade,
+)
+
